3945167f2576b5ecda7adcb6ec72c9eaa9e92335
Wolfgang Mauerer <wolfgang.mauerer@oth-regensburg.de>
2021-04-20 17:31:59 +0200

    Add latency measurement code
    Add a C dispatcher that, given q pre-populated database
    and a query in an external file, measures the execution
    latency distribution.
    
    Signed-off-by: Wolfgang Mauerer <wolfgang.mauerer@oth-regensburg.de>

---
+++ src/latency.c
@@ -0,0 +1,69 @@
+#include <sqlite3.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <fcntl.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <unistd.h>
+#include <sys/mman.h>
+#include "timing.h"
+
+#define ITERATIONS 30
+
+void measure_query(char *dbfile, char *basepath, char *query) {
+    struct stat s;
+    char snum[3];
+    char *err_msg = 0;
+    tstamp_t *tstamps = sqlite3_malloc64(sizeof(tstamp_t)*ITERATIONS);
+    sqlite3 *db;
+
+    int rc = sqlite3_open(dbfile, &db);
+    if (rc != SQLITE_OK) {
+        fprintf(stderr, "Cannot open database: %s\n",
+                sqlite3_errmsg(db));
+        sqlite3_close(db);
+
+	exit(-1);
+    }
+
+    char *file = (char*)sqlite3_malloc64(strlen("query.sql") + strlen(query) + strlen(basepath) + 2);
+    sprintf(file, "%s/query%s.sql", basepath, query);
+
+    int fd = open(file, O_RDONLY);
+    int status = fstat(fd, &s);
+    char *sql = (char *)mmap(0, s.st_size, PROT_READ, MAP_PRIVATE, fd, 0);
+
+    tstamps[0] = get_tstamp();
+    for (int count = 1; count < ITERATIONS; count++) {
+      rc = sqlite3_exec(db, sql, 0, 0, &err_msg);
+      if (rc != SQLITE_OK) {
+        fprintf(stderr, "SQL error: %s\n", err_msg);
+
+        sqlite3_free(err_msg);
+        sqlite3_close(db);
+
+	exit(-1);
+      }
+      tstamps[count]  = get_tstamp();
+    }
+
+    for (int count = 1; count < ITERATIONS; count++) {
+      printf("%s\t%d\t%lu\n", query, count, diff(tstamps[count-1], tstamps[count]));
+    }
+
+    free(tstamps);
+    sqlite3_close(db);
+}
+
+
+int main(int argc, char **argv) {
+    if (argc < 4) {
+      printf("Usage: %s <database> <querypath> q1 q2 ... qn\n", argv[0]);
+      exit(-1);
+    }
+
+    for (int query = 3; query < argc; query++) {
+      measure_query(argv[1], argv[2], argv[query]);
+    }
+}
---
+++ src/timing.h
@@ -0,0 +1,32 @@
+#ifndef INLINE_H
+#define INLINE_H
+
+#include<time.h>
+
+typedef struct timespec tstamp_t;
+
+#define NSEC_PER_SEC 1000000000
+static inline unsigned long diff(tstamp_t start, tstamp_t end)  {
+    tstamp_t temp;
+    if ((end.tv_nsec-start.tv_nsec) < 0) {
+      temp.tv_sec = end.tv_sec-start.tv_sec-1;
+      temp.tv_nsec = NSEC_PER_SEC+end.tv_nsec-start.tv_nsec;
+    } else {
+      temp.tv_sec = end.tv_sec-start.tv_sec;
+      temp.tv_nsec = end.tv_nsec-start.tv_nsec;
+    }
+
+    if (temp.tv_sec != 0) {
+      return((unsigned long)(temp.tv_sec*NSEC_PER_SEC + temp.tv_nsec));
+    }
+
+    return ((unsigned long)temp.tv_nsec);
+}
+
+static inline tstamp_t get_tstamp(void) {
+	tstamp_t val;
+	clock_gettime(CLOCK_THREAD_CPUTIME_ID, &val);
+	return(val);
+    }
+
+#endif
